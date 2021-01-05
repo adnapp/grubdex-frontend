57d405f316f780323ed313c3b57b90c8779348a2
CSWILIK <cswilik@gmail.com>
2021-01-05 12:16:50 -0500

    Working through html

--- src/index.js
+++ src/index.js
@@ -1,15 +1,82 @@
-// Dom Elements 
+// Const
+const ullist = document.querySelector(".ul_list")
+const restaurantDiv = document.querySelector(".restaurants-div")
 
 // Render functions
+
+function renderListofLists(lists) {
+    lists.forEach(list => {
+        const li = document.createElement('li')
+        li.textContent = list.title
+        li.dataset.id = list.id
+        ullist.append(li)
+    })
+}
+
+
+function renderRestaurants(restaurantObj){
+// debugger
+    // restaurantCollection = document.querySelector('.restaurantsDiv')
+    
+    restaurantObj.forEach(restaurant => {
+        const divCard = document.createElement('div')
+    
+        divCard.innerHTML = `
+        <h3>${restaurant.name}</h3>
+        <h4>${restaurant.cuisine}</h4>
+        <h4>${restaurant.address}</h4>
+        <img src=${restaurant.image_url}>
+        <a href="${restaurant.website_url}">Website</a>
+
+        `
+        
+        restaurantDiv.append(divCard)
+        debugger
+    })
+}
 
 
 // Fetch functions
 
-const getOneRestaurant = id => {
-    fetch(`http://localhost:3000/restaurants/${id}`)
+// const getOneRestaurant = id => {
+//     fetch(`http://localhost:3000/restaurants/${id}`)
+//     .then(resp => resp.json())
+//     .then(console.log)
+// }
+
+function getRestaurants() {
+    fetch(`http://localhost:3000/restaurants/`)
     .then(resp => resp.json())
-    .then(console.log)
+    .then(restaurantObj => renderRestaurants(restaurantObj))
 }
 
 
- getOneRestaurant(20)
+function getLists() {
+    fetch('http://localhost:3000/lists')
+    .then(resp => resp.json())
+    .then(lists => renderListofLists(lists))
+}
+
+function getOneList(id) {
+    fetch(`http://localhost:3000/lists/${id}`)
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+
+}
+
+// event listeners
+
+ullist.addEventListener("click", evt => {
+    evt.preventDefault()
+    const id = evt.target.dataset.id 
+    console.log(id)
+    getOneList(id)
+
+})
+
+
+
+
+// initializers 
+getLists()
+  getRestaurants()
